52f031a43829e5d8b7dc74847d94e0060dd3dec5
Joshua <Joshua@DESKTOP-Q6G780M>
2025-11-21 11:53:04 -0600

    Agrege el diseno de la bliblioteca

---
+++ SistemasDeBusquedas/Proyecto/Bliblioteca.cs
@@ -0,0 +1,48 @@
+﻿using SistemasDeBusquedas.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemasDeBusquedas.Proyecto
+{
+    public partial class Bliblioteca : Form
+    {
+        public Bliblioteca()
+        {
+            InitializeComponent();
+        }
+
+        List<Libro> biblioteca = new List<Libro>();
+
+        private void Bliblioteca_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnBuscarTitulo_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnBuscarAutor_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnBuscarCodigo_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btnBuscarCategoria_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
